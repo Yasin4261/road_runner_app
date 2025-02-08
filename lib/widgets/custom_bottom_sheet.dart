68d4dafc67acf3416ca2aa3ff329d7bb505df991
yasin4261 <yasingunesctf@gmail.com>
2025-02-08 04:12:32 +0300

    Fixed theme colors

--- lib/widgets/custom_bottom_sheet.dart
+++ lib/widgets/custom_bottom_sheet.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:road_runner_app/constants/app_theme.dart';
+import 'package:road_runner_app/constants/colors.dart';
 
-// Custom Bottom Sheet Widget
 class CustomBottomSheet extends StatelessWidget {
   const CustomBottomSheet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
-      initialChildSize: 0.2, // Initial size of the bottom sheet
-      minChildSize: 0.2, // Minimum size the bottom sheet can collapse to
-      maxChildSize: 0.3, // Maximum size the bottom sheet can expand to
-      expand: false, // Prevent full-screen expansion
+      initialChildSize: 0.2,
+      minChildSize: 0.2,
+      maxChildSize: 0.3, // Maksimum genişliği artırdım
+      expand: false,
       builder: (context, scrollController) {
         return _BottomSheetContent(scrollController: scrollController);
       },
@@ -18,7 +19,7 @@ class CustomBottomSheet extends StatelessWidget {
   }
 }
 
-// Bottom Sheet Content
+// Bottom Sheet İçeriği
 class _BottomSheetContent extends StatelessWidget {
   final ScrollController scrollController;
 
@@ -27,22 +28,17 @@ class _BottomSheetContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(
-        left: 20.0, // Sol kenara padding
-        right: 20.0, // Sağ kenara padding
-        top: 15.0, // Üst kenara padding
-        bottom: 0.0, // Alt kenara padding yok
-      ),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: AppColors.background, // Arka plan rengini temadan al
         borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(25.0),
           topRight: Radius.circular(25.0),
         ),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 15,
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 10,
             spreadRadius: 2,
           ),
         ],
@@ -61,7 +57,7 @@ class _BottomSheetContent extends StatelessWidget {
   }
 }
 
-// Drag Handle (for the top area)
+// Çekme Çubuğu
 class _DragHandle extends StatelessWidget {
   const _DragHandle();
 
@@ -72,7 +68,7 @@ class _DragHandle extends StatelessWidget {
         width: 50,
         height: 5,
         decoration: BoxDecoration(
-          color: Colors.grey[400],
+          color: Colors.grey[600], // Daha koyu bir renk kullan
           borderRadius: BorderRadius.circular(10),
         ),
       ),
@@ -80,24 +76,24 @@ class _DragHandle extends StatelessWidget {
   }
 }
 
-// Title of the Bottom Sheet
+// Başlık
 class _Title extends StatelessWidget {
   const _Title();
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
+    return Text(
       "Sipariş Detayları",
-      style: TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-        color: Colors.black87,
-      ),
+      style: Theme.of(context).textTheme.displayLarge?.copyWith(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: AppColors.textPrimary,
+          ),
     );
   }
 }
 
-// List of Orders inside the Bottom Sheet
+// Sipariş Listesi
 class _OrderList extends StatelessWidget {
   final ScrollController scrollController;
 
@@ -121,7 +117,7 @@ class _OrderList extends StatelessWidget {
   }
 }
 
-// Order Card Widget
+// Sipariş Kartı
 class _OrderCard extends StatelessWidget {
   final int index;
 
@@ -130,40 +126,51 @@ class _OrderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Theme.of(context).cardTheme.color, // Temadan al
       margin: const EdgeInsets.symmetric(vertical: 8),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15),
       ),
       elevation: 4,
       child: ListTile(
-        contentPadding:
-            const EdgeInsets.all(12), // Slightly larger padding for more space
+        contentPadding: const EdgeInsets.all(12),
         leading: const _OrderIcon(),
         title: Text(
           "Sipariş #${1000 + index}",
-          style: const TextStyle(
+          style: TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.bold,
+            color: AppColors.textPrimary, // Metin rengini temadan al
           ),
         ),
-        subtitle: const Text("Adres: İstanbul, Kadıköy"),
-        trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-        // Adding a new Row to display delay time below the subtitle
-        isThreeLine: true, // Allows 3 lines of content
-        dense: false, // Makes the widget more compact if set to true
-        // Add delay information as a Row below subtitle
+        subtitle: Text(
+          "Adres: İstanbul, Kadıköy",
+          style: TextStyle(color: AppColors.textSecondary),
+        ),
+        trailing:
+            const Icon(Icons.arrow_forward_ios, size: 16, color: Colors.white),
+        isThreeLine: true,
+        dense: false,
         onTap: () {
           showDialog(
             context: context,
             builder: (BuildContext context) => AlertDialog(
-              title: const Text("Sipariş Gecikmesi"),
-              content: const Text("Bu sipariş 30 dakika gecikmiş olabilir."),
+              backgroundColor: AppColors.background, // Temaya uygun hale getir
+              title: Text(
+                "Sipariş Gecikmesi",
+                style: TextStyle(color: AppColors.textPrimary),
+              ),
+              content: Text(
+                "Bu sipariş 30 dakika gecikmiş olabilir.",
+                style: TextStyle(color: AppColors.textSecondary),
+              ),
               actions: <Widget>[
                 TextButton(
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
-                  child: const Text("Tamam"),
+                  child:
+                      Text("Tamam", style: TextStyle(color: AppColors.primary)),
                 ),
               ],
             ),
@@ -174,7 +181,7 @@ class _OrderCard extends StatelessWidget {
   }
 }
 
-// Order Icon Widget (used in each list item)
+// Sipariş İkonu
 class _OrderIcon extends StatelessWidget {
   const _OrderIcon();
 
@@ -182,9 +189,9 @@ class _OrderIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return CircleAvatar(
       radius: 30,
-      backgroundColor: Colors.blueAccent,
+      backgroundColor: AppColors.primary,
       child: const Icon(
-        Icons.shopping_bag, // Icon used for the order
+        Icons.shopping_bag,
         size: 30,
         color: Colors.white,
       ),
